08c9a739ecc23d1279450c5268e1d9c5fc101960
Andrew Meadows <andrew@highfidelity.io>
2020-07-21 21:13:48 -0700

    cleaner solar.py

--- solar.py
+++ solar.py
@@ -2,13 +2,13 @@
 #
 # compute solar-array and battery capacity
 
-# Assume the batteries get their most workout at night
-# when there is no sun.  This determines the battery capacity.
-hours_of_night = 14
-nighttime_power_consumption = 100 # watts
-nighttime_energy_consumption = hours_of_night * nighttime_power_consumption # watt-hours
+# set these parameters according to the average of your worst week
+hours_with_sunlight = 6 
+hours_no_sunlight = 24 - hours_with_sunlight
+sunless_power_consumption = 100 # watts
+sunny_power_consumption = 100 # watts
 
-# Batteries need to supply all of nighttime energy, and then some.
+# Batteries need to supply all of sunless_energy, and then some.
 # This to prevent battery deterioration.
 # For lithium batteries we can use a safety factor of a little over 1.0
 # (they can be nearly completely depleted and still be fine).
@@ -19,27 +19,28 @@ if (battery_type == "lithium"):
     BATTERY_SAFETY_FACTOR = 1.2
 else:
     BATTERY_SAFETY_FACTOR = 2.0
-battery_capacity = BATTERY_SAFETY_FACTOR * nighttime_energy_consumption
 
 # set the battery voltage according to the batteries you're thinking of buying
 # lead-acid deep cycle batteries are usually 12V per cell
 # but can sometimes come in 6V and 24V
 # lithium battery packs come in more variable configurations
 battery_voltage = 12 # volts
+
+# compute battery capacity as "amp-hours at aforementioned voltage"
+sunless_energy_consumption = hours_no_sunlight * sunless_power_consumption # watt-hours
+battery_capacity = BATTERY_SAFETY_FACTOR * sunless_energy_consumption
 battery_amp_hours = battery_capacity / battery_voltage
 
 # Now compute solar array size
-hours_of_sunlight = 6
-daytime_power_consumption = 100 # watts
-daytime_energy_consumption = (24 - hours_of_night) * daytime_power_consumption
+sunny_energy_consumption = hours_with_sunlight * sunny_power_consumption
 
-total_energy_consumption = nighttime_energy_consumption + daytime_energy_consumption
+total_energy_consumption = sunless_energy_consumption + sunny_energy_consumption
 
-solar_power = total_energy_consumption / hours_of_sunlight
+solar_power = total_energy_consumption / hours_with_sunlight
 
 
-print("nighttime power = {}".format(nighttime_power_consumption))
-print("daytime power = {}".format(daytime_power_consumption))
+print("sunless_power = {}".format(sunless_power_consumption))
+print("sunny power = {}".format(sunny_power_consumption))
 print("battery_type = {}".format(battery_type))
 print("battery_voltage = {} volts".format(battery_voltage))
 print("battery_amp_hours = {} amp-hours".format(battery_amp_hours))
